4bb3a8d5d566764005919cd9782ab3f7dc8da5b5
devtrev <dev.trev22@hotmail.com>
2020-07-05 09:15:12 -0500

    Initial Commit
    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MOLE
 Monitoring OnLine Endpoints - This will be used to check health of URLs and report back on status
+
+
+urlCheck.py 
+    This is intended to be run as cron job or run every X minutes to loop through list of endpoints and test if they are able to connect or not. Currently just prints to logs the status of the job.
---
+++ urlCheck.py
@@ -0,0 +1,25 @@
+import urllib.request, urllib.error
+
+print("Docker is magic!")
+
+urlList = dict({
+'Google': 'http://www.google.com', 
+'Google_bad': 'http://www.google.com/aaaa',
+'Test_bad': 'https://www.hkkrdsdoociffs.com',
+'Google_https': 'https://www.google.com' 
+}) 
+
+for key, url in urlList.items():
+    try:
+        conn = urllib.request.urlopen(url)
+    except urllib.error.HTTPError as e:
+    	# Return code error (e.g. 200, 404, 501, ...)
+    	print(key + ' : {}'.format(e.code))
+    except urllib.error.URLError as e:
+    	# Not an HTTP-specific error (e.g. connection refused)
+    	# ...
+    	print(key + ' : {}'.format(e.reason))    
+    else :
+    	# Not an HTTP-specific error (e.g. connection refused)
+    	# ...
+        print(key + ' : 200')
